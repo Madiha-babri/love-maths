205c9b824cb89fa036f376db18d748721b166081
Madiha Babri <mybabri2@gmail.com>
2024-11-06 00:45:51 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
 /**
@@ -21,10 +23,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
  
-function runGame() {
+function runGame(gameType) {
     // create two random numbers between 1 and 25 //
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gametype}`);
+        throw `Unknown game type: ${gametype}. Aborting!`;
+    }
+    
 
 }
 
@@ -48,12 +58,14 @@ function displayAdditionQuestion() {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
 
 }
 
 function displayMultiplyQuestion() {
-    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displayDivisionQuestion() {
