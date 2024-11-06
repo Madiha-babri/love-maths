f948964fb09835c3ced9a0fdfbaaa6bb67740cb2
Madiha Babri <mybabri2@gmail.com>
2024-11-06 00:10:34 +0000

    set up structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+function runGame () {
+
+}
+function checkAnswer() {
+    
+}
+function calculateCorrectAnswer() {
+    
+}
+function incrementScore() {
+    
+}
+function incrementWrongAnswer() {
+    
+}
+function displayAdditionQuestion() {
+    
+}
+function displaySubtractQuestion() {
+    
+}
+function displayMulitplyQuestion() {
+    
+}
+
+function displayDivisionQuestion() {
+    
+}
